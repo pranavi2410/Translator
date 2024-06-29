0bfa1cb14c9b2dd10b65c109fa601fe954f9c53f
pranavi2410 <120576398+pranavi2410@users.noreply.github.com>
2024-06-29 19:31:51 +0530

    Add files via upload

---
+++ translator/main/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.shortcuts import HttpResponse 
+from translate import Translator 
+  
+def home(request): 
+    if request.method == "POST": 
+        text = request.POST["translate"] 
+        language = request.POST["language"] 
+        translator= Translator(to_lang=language) 
+        translation = translator.translate(text) 
+        return HttpResponse(translation) 
+    return render(request,"main/index.html")
